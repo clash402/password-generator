fc5618acfdcecddc6265faf0cf6ddef5bbfcf016
Josh Courtney <joshrcourtney@gmail.com>
2021-01-14 14:10:38 -0600

    Refactor main.py to streamline user input

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
-#Password Generator Project
 import random
 
+
 # PROPERTIES
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+# METHODS
+def generate_random_chars(count, chars):
+    return [random.choice(chars) for _ in range(count)]
+
+
+def flatten_list(nested_list):
+    return [item for sublist in nested_list for item in sublist]
 
 
 # MAIN
 print("Welcome to the PyPassword Generator")
 
-nr_letters= int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+char_count = int(input("How many characters would you like in your password?\n"))
 
-password = []
+letter_count = random.randint(0, char_count)
+char_count -= letter_count
+number_count = random.randint(0, char_count)
+char_count -= number_count
+symbol_count = char_count
 
-for i in range(nr_letters):
-  password.append(random.choice(letters))
-for i in range(nr_symbols):
-  password.append(random.choice(symbols))
-for i in range(nr_numbers):
-  password.append(random.choice(numbers))
+password = generate_random_chars(letter_count, letters)
+password.append(generate_random_chars(number_count, numbers))
+password.append(generate_random_chars(symbol_count, symbols))
+password = flatten_list(password)
 
 random.shuffle(password)
 password = ''.join(password)
